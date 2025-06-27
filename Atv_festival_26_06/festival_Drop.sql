13c77b0e8e3dbb13050d6052a6d06f53455492f1
Rafael da Silva Ferreira <146126647+elchukito@users.noreply.github.com>
2025-06-26 22:27:46 -0300

    Create festival_Drop.sql

---
+++ Atv_festival_26_06/festival_Drop.sql
@@ -0,0 +1,10 @@
+-- drop das tabelas filho e relacionadas
+DROP TABLE IF EXISTS festival_Aluno_Apresentacao;
+DROP TABLE IF EXISTS festival_Professor_Apresentacao;
+DROP TABLE IF EXISTS festival_Apresentacao;
+
+-- drop da outras tabelas
+DROP TABLE IF EXISTS festival_Tp_apresentacao;
+DROP TABLE IF EXISTS festival_Festival;
+DROP TABLE IF EXISTS festival_Professor;
+DROP TABLE IF EXISTS festival_Aluno;
