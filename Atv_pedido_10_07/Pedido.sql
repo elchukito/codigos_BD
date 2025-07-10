82885053def5a10cd768189db1954933d729eedd
Rafael da Silva Ferreira <146126647+elchukito@users.noreply.github.com>
2025-07-10 11:23:42 -0300

    Create Table Pedido

--- Atv_pedido_10_07/Pedido.sql
+++ Atv_pedido_10_07/Pedido.sql
@@ -1,7 +1,14 @@
+DROP TABLE IF EXISTS pedido_Pedido;
 DROP TABLE IF EXISTS pedido_Produto;
 
 CREATE TABLE pedido_Produto (
 	id_produto_pk INT PRIMARY KEY AUTO_INCREMENT,
     nome_produto VARCHAR(100) NOT NULL,
     preco DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE pedido_Pedido (
+	id_pedido_pk INT PRIMARY KEY AUTO_INCREMENT,
+	data_pedido DATE NOT NULL,
+	cliente_nome VARCHAR(100)
 );
