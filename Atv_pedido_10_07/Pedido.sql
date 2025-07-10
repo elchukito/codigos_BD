27f9abc713235d49b2cfca434c512e6b50383c15
Rafael da Silva Ferreira <146126647+elchukito@users.noreply.github.com>
2025-07-10 11:34:35 -0300

    Update Pedido.sql

--- Atv_pedido_10_07/Pedido.sql
+++ Atv_pedido_10_07/Pedido.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS pedido_Item_pedido;
 DROP TABLE IF EXISTS pedido_Pedido;
 DROP TABLE IF EXISTS pedido_Produto;
 
@@ -9,6 +10,16 @@ CREATE TABLE pedido_Produto (
 
 CREATE TABLE pedido_Pedido (
 	id_pedido_pk INT PRIMARY KEY AUTO_INCREMENT,
-	data_pedido DATE NOT NULL,
-	cliente_nome VARCHAR(100)
+    data_pedido DATE NOT NULL,
+    cliente_nome VARCHAR(100)
 );
+
+CREATE TABLE pedido_Item_pedido (
+	id_pedido_fk INT,
+    id_produto_fk INT,
+    quantidade_ip INT NOT NULL,
+    preco_unitario_ip DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (id_pedido_fk, id_produto_fk),
+    FOREIGN KEY (id_pedido_fk) REFERENCES pedido(id_pedido_pk),
+    FOREIGN KEY (id_produto_fk) REFERENCES produto(id_produto_pk)
+)
